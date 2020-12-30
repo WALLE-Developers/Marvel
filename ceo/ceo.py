d6f9851417287ad9a78515402efa70b7b79537eb
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-30 19:45:22 +0000

    Create ceo.py

---
+++ ceo/ceo.py
@@ -0,0 +1,14 @@
+import discord
+import random
+from redbot.core import commands
+
+class CEO(commands.Cog):
+    """How much of a CEO are you?"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ceo(self, ctx):
+        """How much of a CEO are you?"""
+        await ctx.send("{ctx.author.name}, you're gonna be a **{random.randint(1, 100)}% CEO** in your next crystal opening.")
