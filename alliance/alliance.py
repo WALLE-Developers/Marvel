b909ee8208b2d5da2ebe5967f0b8ecd4d790e5e6
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-30 10:46:37 +0000

    Update alliance.py

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -1,8 +1,7 @@
 import discord
 from redbot.core import commands, checks, Config
 
-TZ = [
-  'PST', 'MST', 'CST', 'EST', 'BST', 'GMT', 'UTC', 'CET', 'MSK', 'GST', 'IST', 'SST', 'CST', 'JST', 'AEDT', 'NZDT', 'pst', 'mst', 'cst', 'est', 'bst', 'gmt', 'utc', 'cet', 'msk', 'gst', 'ist', 'sst', 'cst', 'jst', 'aedt', 'nzdt']
+TZ = 'PST', 'MST', 'CST', 'EST', 'BST', 'GMT', 'UTC', 'CET', 'MSK', 'GST', 'IST', 'SST', 'CST', 'JST', 'AEDT', 'NZDT', 'pst', 'mst', 'cst', 'est', 'bst', 'gmt', 'utc', 'cet', 'msk', 'gst', 'ist', 'sst', 'cst', 'jst', 'aedt', 'nzdt'
 
 class Alliance(commands.Cog):
   """Alliance commands for MCOC."""
