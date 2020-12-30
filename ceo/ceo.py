06f948ad87320da1d744bbb6c02f495b510cf59c
kreusada <67752638+kreus7@users.noreply.github.com>
2020-12-30 19:52:12 +0000

    Update ceo.py

--- ceo/ceo.py
+++ ceo/ceo.py
@@ -2,6 +2,12 @@ import discord
 import random
 from redbot.core import commands
 
+responses = [
+    ", according to my calculations, you will be a",
+    ", you're gonna be a",
+    ", I think you're gonna end up as a"
+]
+
 class CEO(commands.Cog):
     """How much of a CEO are you?"""
 
@@ -11,4 +17,4 @@ class CEO(commands.Cog):
     @commands.command()
     async def ceo(self, ctx):
         """How much of a CEO are you?"""
-        await ctx.send(f"{ctx.author.name}, you're gonna be a **{random.randint(1, 100)}% CEO** in your next crystal opening.")
+        await ctx.send(f"{ctx.author.name}{random.choice(responses)} **{random.randint(1, 100)}% CEO** in your next crystal opening.")
