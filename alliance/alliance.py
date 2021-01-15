0cd177bb644005de329b995a044652b1d603863e
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-15 13:05:08 +0000

    Merge CEO into alliance

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -15,6 +15,30 @@ class Alliance(commands.Cog):
   @commands.group()
   async def allianceset(self, ctx):
     """Setup for your alliance."""
+
+responses = [
+    ", according to my calculations, you will be a",
+    ", you're gonna be a",
+    ", I think you're gonna end up as a"
+]
+
+  @commands.command()
+  @commands.cooldown(1, 300, commands.BucketType.user)
+  async def ceo(self, ctx):
+      """How much of a CEO are you?"""
+      ceo = random.randint(1, 100)
+      currency = await bank.get_currency_name(ctx.guild)
+      if ceo == 42:
+          await ctx.send(f"**42%**, nice {ctx.author.name}!! I added 42,000 {currency} to your bank account.")
+          await bank.deposit_credits(ctx.author, 42000)
+      elif ceo == 69:
+          await ctx.send(f"**69%**, oooo yeahh. I added 69,000 {currency} to your bank account, {ctx.author.name}.")
+          await bank.deposit_credits(ctx.author, 69000)
+      elif ceo == 100:
+          await ctx.send(f"**100%** wooooow {ctx.author.name} I added 100,000 {currency} to your bank account.")
+          await bank.deposit_credits(ctx.author, 100000)
+      else:
+          await ctx.send(f"{ctx.author.name}{random.choice(responses)} **{ceo}% CEO** in your next crystal opening.")
     
   @commands.command()
   @commands.is_owner()
