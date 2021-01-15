45d507269aad6ed0c28b68c41c3d4c057e04058b
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-15 13:06:34 +0000

    modules

--- alliance/alliance.py
+++ alliance/alliance.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from redbot.core import commands, checks, Config
 
 TZ = 'PST', 'MST', 'CST', 'EST', 'BST', 'GMT', 'UTC', 'CET', 'MSK', 'GST', 'IST', 'SST', 'CST', 'JST', 'AEDT', 'NZDT', 'pst', 'mst', 'cst', 'est', 'bst', 'gmt', 'utc', 'cet', 'msk', 'gst', 'ist', 'sst', 'cst', 'jst', 'aedt', 'nzdt'
