b994c46a03b3e28f52e18234c9c7a10f8fee40d1
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-21 22:15:14 +0000

    Initial commit

--- marvel/__init__.py
+++ marvel/__init__.py
@@ -1,4 +1,4 @@
-from .core import Marvel
+import json; from pathlib import Path; from .core import setup
 
-def setup(bot):
-  bot.add_cog(Marvel(bot))
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
