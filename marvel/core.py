990104124a6d7528f39ecba87b7eb0219897e464
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-21 21:56:43 +0000

    Core updates

--- marvel/core.py
+++ marvel/core.py
@@ -4,13 +4,6 @@ from .alliance import Alliance
 IDENTIFIER = 3249832743924
 
 class CompositeMetaClass(type(commands.Cog), type(ABC)):
-    """
-    This allows the metaclass used for proper type detection to
-    coexist with discord.py's metaclass
-    This is from
-    https://github.com/Cog-Creators/Red-DiscordBot/blob/V3/develop/redbot/cogs/mod/mod.py#L23
-    """
-
     pass
     
 class Marvel(Alliance, commands.Cog, metaclass=CompositeMetaClass):
