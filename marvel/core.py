449069deb938dab7210fdd130c48db6697301b0b
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-21 21:56:19 +0000

    Updates to core

--- marvel/core.py
+++ marvel/core.py
@@ -1,8 +1,7 @@
 from abc import ABC
-
-from redbot.core import commands, checks, Config
-
 from .alliance import Alliance
+
+IDENTIFIER = 3249832743924
 
 class CompositeMetaClass(type(commands.Cog), type(ABC)):
     """
@@ -15,6 +14,19 @@ class CompositeMetaClass(type(commands.Cog), type(ABC)):
     pass
     
 class Marvel(Alliance, commands.Cog, metaclass=CompositeMetaClass):
-  """Marvel commands built for WALL-E."""
-  
-  
+    """Marvel commands built for WALL-E."""
+    
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = Config.get_conf(
+            self, 
+            IDENTIFIER, 
+            force_registration=True
+        )
+        self.config.register_guild(
+            timezone=None, 
+            officerrole=None
+        )
+        self.config.register_user(
+            tzon=False
+        )
