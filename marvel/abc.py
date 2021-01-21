9c57fd56fdf340507c093340efec0a9e14fc889e
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-21 21:50:54 +0000

    Initial commit

---
+++ marvel/abc.py
@@ -0,0 +1,14 @@
+from abc import ABC
+from typing import Dict
+
+from redbot.core import Config
+from redbot.core.bot import Red
+
+class MixinMeta(ABC):
+
+    def __init__(self, *_args):
+        self.settings: Config
+        self.bot: Red
+        self.sanitize: Dict[str, bool]
+        
+# Mixin class used to string together the package.
