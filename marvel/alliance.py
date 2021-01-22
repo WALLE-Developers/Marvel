2f5cfb6d2b12fce3fb4e93fcb1115df720af60fc
Kreusada <kreusadaprojects@gmail.com>
2021-01-22 13:21:28 +0000

    Removed all alliance commands for now

--- marvel/alliance.py
+++ marvel/alliance.py
@@ -7,6 +7,6 @@ from redbot.core import commands, checks, Config, bank
 class Alliance(MixinMeta):
     """A collection of alliance commands built for WALL-E."""
     
-    @commands.group()
-    async def allianceset(self, ctx):
-        """Setup for your alliance."""
+#    @commands.group()
+#    async def allianceset(self, ctx):
+#        """Setup for your alliance."""
