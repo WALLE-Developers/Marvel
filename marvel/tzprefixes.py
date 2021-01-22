99de73d14860ddfc5270a0d59d6990eb085b95ee
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-22 09:06:07 +0000

    Seperate file should sort the ABCMeta issue

---
+++ marvel/tzprefixes.py
@@ -0,0 +1,6 @@
+TZ = (
+    'PST', 'MST', 'CST', 'EST', 'BST', 'GMT', 'UTC', 'CET',
+    'MSK', 'GST', 'IST', 'SST', 'CST', 'JST', 'AEDT', 'NZDT',
+    'pst', 'mst', 'cst', 'est', 'bst', 'gmt', 'utc', 'cet', 
+    'msk', 'gst', 'ist', 'sst', 'cst', 'jst', 'aedt', 'nzdt'
+)
