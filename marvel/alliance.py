af65d10bac30acaa83c08bb4933e4c4a1f04f095
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-22 09:28:26 +0000

    Commented off alliance features temporarily

--- marvel/alliance.py
+++ marvel/alliance.py
@@ -7,13 +7,13 @@ from redbot.core import commands, checks, Config, bank
 class Alliance(MixinMeta):
     """A collection of alliance commands built for WALL-E."""
     
-    @commands.group()
-    async def allianceset(self, ctx):
-        """Setup for your alliance."""
+#    @commands.group()
+#    async def allianceset(self, ctx):
+#        """Setup for your alliance."""
             
-    @allianceset.command()
-    @commands.has_permissions(manage_roles=True)
-    async def officer(self, ctx, role: discord.Role):
-        """Set your officer role."""
-        await self.config.guild(ctx.guild).officerrole.set(role.id)
-        await ctx.send(f"Done. {role.mention} will now be considered as an alliance officer role.")
+#    @allianceset.command()
+#    @commands.has_permissions(manage_roles=True)
+#    async def officer(self, ctx, role: discord.Role):
+#        """Set your officer role."""
+#        await self.config.guild(ctx.guild).officerrole.set(role.id)
+#        await ctx.send(f"Done. {role.mention} will now be considered as an alliance officer role.")
