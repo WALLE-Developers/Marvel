2262b6f8fab3b4a1e529359c8034f0d2b994f246
kreusada <67752638+kreus7@users.noreply.github.com>
2021-01-25 22:01:25 +0000

    [Core] Removed alliance import

--- marvel/core.py
+++ marvel/core.py
@@ -4,7 +4,6 @@ from abc import ABC
 from redbot.core import commands, Config
 from redbot.core.bot import Red
 
-from .alliance import Alliance
 from .tz import TZ
 from .ceo import CEO
 from .mjolnir import Mjolnir
